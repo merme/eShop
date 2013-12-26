608d3005cc7ea28e7f5001a53576995953556b6c
JaCaLla <javi.calatrava@gmail.com>
2013-12-26 11:50:29 +0100

    Update a product from the list

---
+++ eShop/ViewControllers/EditProductVC.h
@@ -0,0 +1,13 @@
+//
+//  EditProductVC.h
+//  eShop
+//
+//  Created by JAVIER CALATRAVA LLAVERIA on 26/12/13.
+//  Copyright (c) 2013 JAVIER CALATRAVA LLAVERIA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditProductVC : UIViewController
+
+@end
---
+++ eShop/ViewControllers/EditProductVC.m
@@ -0,0 +1,38 @@
+//
+//  EditProductVC.m
+//  eShop
+//
+//  Created by JAVIER CALATRAVA LLAVERIA on 26/12/13.
+//  Copyright (c) 2013 JAVIER CALATRAVA LLAVERIA. All rights reserved.
+//
+
+#import "EditProductVC.h"
+
+@interface EditProductVC ()
+
+@end
+
+@implementation EditProductVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
